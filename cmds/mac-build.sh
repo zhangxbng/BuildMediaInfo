c3b6a3300c53b25448e483453857b30a39d1f354
ZHANG XIAOBING <zhangxbng@gmail.com>
2021-05-30 17:54:53 +0800

    !R: tweak for mac

--- cmds/mac-build.sh
+++ cmds/mac-build.sh
@@ -1,15 +1,5 @@
-#/Applications/CMake.app/Contents/bin/cmake -S "../MediaInfoLib/Project/CMake"  -B "./build/mediainfolib"
-
-#/Applications/CMake.app/Contents/bin/cmake -S "../zlib"  -B "./build/zlib"
-#make -C "./build/zlib"
-
-#/Applications/CMake.app/Contents/bin/cmake -S "../ZenLib/Project/CMake"  -B "./build/zenlib" -DBUILD_SHARED_LIBS=OFF
-#make -C "./build/zenlib"
-
-#/Applications/CMake.app/Contents/bin/cmake -S "../MediaInfoLib/Project/CMake"  -B "./build/mediainfolib" -DBUILD_SHARED_LIBS=OFF -DZenLib_DIR=$(pwd)/./build/zenlib -DZenLib_INCLUDE_DIR=$(pwd)/../ZenLib/Source
-#make -C "./build/mediainfolib"
-
-
-#/Applications/CMake.app/Contents/bin/cmake -S "../MediaInfoLib/Project/CMake"  -B "./build/mediainfolib" -DBUILD
-
-/Applications/CMake.app/Contents/bin/cmake -B "./build/mediainfolib"
+rm -rf build
+mkdir -p build/mac
+cd build/mac
+/Applications/CMake.app/Contents/bin/cmake ../../
+make
